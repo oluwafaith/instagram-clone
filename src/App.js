ff3c147f75fc4e43f850a4569b2865f84d169736
Okpala Faith <okpalafaith1@gmail.com>
2021-02-23 17:13:41 +0100

    added the routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import React, { lazy, suspense } from 'react';
 
-import './App.css';
+const Dashboard = lazy(() => import ('./pages/dashboard'));
+const Login = lazy(() => import ('./pages/login'));
+const SignUp = lazy(() => import ('./pages/signup'));
+const Profile = lazy(() => import ('./pages/profile'));
+const NotFound = lazy(() => import ('./pages/not-found'));
 
 function App() {
   return (
