640d0c5faa797b9a14247ef8e6efffc323316034
Okpala Faith <okpalafaith1@gmail.com>
2021-02-24 08:59:11 +0100

    login form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { lazy, suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import * as ROUTES from './constants/routes';
 const Dashboard = lazy(() => import ('./pages/dashboard'));
 const Login = lazy(() => import ('./pages/login'));
 const SignUp = lazy(() => import ('./pages/signup'));
 const Profile = lazy(() => import ('./pages/profile'));
 const NotFound = lazy(() => import ('./pages/not-found'));
 
-function App() {
-  return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg ">
-    <div className="">
-      <div className="">
-        Blessing Krofegha
-      </div>
-      <p className="">
-        When I’m not coding, I switch to Netflix with biscuits and cold tea as my companion. <span></span>😜
-      </p>
-    </div>
-    <div className="">
-      <span className="">#Software Engineer</span>
-      <span className="">#Writter</span>
-      <span className="">#Public Speaker</span>
-    </div>
-  </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Switch>
+                    <Route path={ROUTES.LOGIN} component={Login} />
+                    <Route path={ROUTES.SIGN_UP} component={SignUp} />
+                    <Route path={ROUTES.PROFILE} component={Profile} />
+                    <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+                    <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+                </Switch>
+            </Suspense>
+        </Router>
+    );
 }
-
-export default App;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,84 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
+    const history = useHistory();
+
+    const { firebase } = useContext(FirebaseContext);
+
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+    
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        
+        try {
+            await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+            history.push(ROUTES.DASHBOARD);
+        } catch (error) {
+            setEmailAddress('');
+            setPassword('');
+            setError(error.message);
+        }
+    }
+
+    useEffect(()=>{
+        document.title=  'Login - Instagram';
+    },[])
+
     return (
-        <p>Hello from Login</p>
+<div className="container flex mx-auto max-w-screen-md items-center h-screen">
+<div className="flex w-3/5">
+    <img src="/images/iphone-with-profile3.jpg" alt="iPhone with Instagram app" />
+</div>
+<div className="flex flex-col w-2/5">
+<div className="flex flex-col items-center bg-white p-4 border mb-4">
+    <h1 className="flex justify-center w-full">
+        <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+    </h1>
+    {error && <p className="mb-4 text-xs text-red-500">{error}</p>}
+     <form onSubmit={handleLogin} method="POST">
+                    <input
+                        aria-label="Enter your email address"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+                        type="text"
+                        placeholder="Email address"
+                        value={emailAddress}
+                        onChange={({ target }) => setEmailAddress(target.value)}
+                    />
+                    <input
+                        aria-label="Enter your password"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                    <button
+                        disabled={isInvalid}
+                        type="submit"
+                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${ isInvalid && 'cursor-not-allowed opacity-50'
+                        }`}
+                    >
+                        Log In
+                    </button>
+                </form>
+</div>
+    <div className="flex justify-center items-center flex-col w-full bg-white p-4 border">
+        <p className="text-sm">
+            Don't have an account?{' '}
+            <Link to={ROUTES.SIGN_UP} className="font-bold">
+                Sign up
+            </Link>
+        </p>
+    </div>
+</div>
+</div>
+
     )
 }
